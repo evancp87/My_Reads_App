bbe16f58ab24445b2b77e588384bce0ae33f9cb3
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-09-06 10:25:39 +0100

    typo fixed for read shelf

--- src/ShelfPicker.js
+++ src/ShelfPicker.js
@@ -25,7 +25,7 @@ const {book} = this.props;
           </option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
-          <option value="alreadyRead">Read</option>
+          <option value="read">Read</option>
           <option value="none">None</option>
         </select>
       
